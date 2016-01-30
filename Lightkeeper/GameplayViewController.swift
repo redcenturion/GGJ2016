9d5c4fb881181e7c24206c37722b3fa205cd5447
skewerb <skewerblack@gmail.com>
2016-01-30 11:10:38 -0800

    add the actual gameplay files

--- Lightkeeper/GameplayViewController.swift
+++ Lightkeeper/GameplayViewController.swift
@@ -6,8 +6,35 @@
 //  Copyright © 2016 GGJ2016. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
 class GameplayViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("GameplayView is loading")
+    }
+    
+    override func shouldAutorotate() -> Bool {
+        return true
+    }
+    
+    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
+        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
+            return .AllButUpsideDown
+        } else {
+            return .All
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
 
 }
