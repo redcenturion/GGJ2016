cb28627830df442307ad5841b3b62d9dbbfcfdb5
Peter Huynh <peterhuynh0@outlook.com>
2016-01-30 11:02:06 -0800

    The second commit

---
+++ Lightkeeper/MainMenuScene.swift
@@ -0,0 +1,16 @@
+//
+//  MainMenuScene.swift
+//  Lightkeeper
+//
+//  Created by Peter Huynh on 1/30/16.
+//  Copyright © 2016 GGJ2016. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class MainMenuScene: SKScene {
+    var playButton = SKSpriteNode()
+    let platButtonText = SKTexture(imageNamed: "play")
+    
+}
