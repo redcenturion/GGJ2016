8c756ebb3da39956fe991d4be4711e09daba561a
skewerb <skewerblack@gmail.com>
2016-01-30 11:10:27 -0800

    rm template files, add gameplay scene

---
+++ Lightkeeper/GameplayScene.swift
@@ -0,0 +1,9 @@
+//
+//  GameplayScene.swift
+//  Lightkeeper
+//
+//  Created by Steven Yang on 1/30/16.
+//  Copyright © 2016 GGJ2016. All rights reserved.
+//
+
+import Foundation
---
+++ Lightkeeper/GameplayViewController.swift
@@ -0,0 +1,13 @@
+//
+//  GameplayViewController.swift
+//  Lightkeeper
+//
+//  Created by Steven Yang on 1/30/16.
+//  Copyright © 2016 GGJ2016. All rights reserved.
+//
+
+import Cocoa
+
+class GameplayViewController: UIViewController {
+
+}
