ce177ce38d292e76a6bef585581ce51753a23c6d
Peter Huynh <peterhuynh0@outlook.com>
2016-01-30 11:12:57 -0800

    3rd commmit

---
+++ Lightkeeper/MainMenuController.swift
@@ -0,0 +1,23 @@
+//
+//  MainMenuController.swift
+//  Lightkeeper
+//
+//  Created by Peter Huynh on 1/30/16.
+//  Copyright © 2016 GGJ2016. All rights reserved.
+//
+
+import SpriteKit
+import UIKit
+
+class SwiftStudyViewController: UIViewController {
+    
+    init(nibName nibNameOrNil: String!, bundle nibBundleOrNil: NSBundle!) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+    }
+    
+}
